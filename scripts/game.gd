0e2fadc18c8316493ebe0419e89d50516c5c31b5
roxsas64 <roxsas64@gmail.com>
2025-08-15 21:08:18 +0200

    Minor changes

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,11 +6,8 @@ signal omamori_changed
 signal omamori_clicked
 
 func _ready() -> void:
-	#load_data()
 	pass
-
 
 func _on_click_button_button_down() -> void:
 	GameManager.addOmamori(amount_per_click)
 	emit_signal("omamori_clicked", amount_per_click)
-	#save_data()
