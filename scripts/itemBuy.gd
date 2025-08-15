6e6ae6e7b1c38ad3c5877d233d9f7a4209efa962
roxsas64 <roxsas64@gmail.com>
2025-08-15 21:07:23 +0200

    Add @tool to Item Buy

--- scripts/itemBuy.gd
+++ scripts/itemBuy.gd
@@ -1,12 +1,29 @@
+@tool
 extends Control
-
-@export var itemName : String
-@export var price : int
-@export var itemTexture : Texture2D
 
 @onready var itemLabel : Label = $ItemLabel
 @onready var priceTag : Label = $PriceTag
 @onready var itemIcon : Sprite2D = $ItemIcon
+
+@export var itemName: String:
+	set(value):
+		itemName = value
+		if  is_inside_tree():
+			$ItemLabel.text = itemName
+			
+@export var price: int:
+	set(value):
+		price = value
+		if  is_inside_tree():
+			$PriceTag.text = str(value)
+			
+@export var itemTexture: Texture2D:
+	set(value):
+		itemTexture = value
+		if is_inside_tree():
+			$ItemIcon.texture = itemTexture
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,14 +31,12 @@ func _ready() -> void:
 	priceTag.text = str(price) + " dolale"
 	itemIcon.texture = itemTexture
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
 
 func _on_buy_pressed() -> void:
 	if GameManager.omamori >= price:
 		# comprable
 		GameManager.minusOmamori(price)
+		Audiomanager.buyItem.play()
